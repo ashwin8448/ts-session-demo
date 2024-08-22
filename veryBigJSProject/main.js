fab2986a1083eb3b31d5e25cdcd505c98b1e1a9f
Ashwin Nair <ashwin.nair@qburst.com>
2024-08-22 14:24:36 +0530

    minor change

--- veryBigJSProject/main.js
+++ veryBigJSProject/main.js
@@ -20,38 +20,19 @@ const users = [
 
 function getUser(userId) {
   try {
-    let user = users[userId];
-    console.log(user.name);
-    userDetails.innerText = `${user.name} is ${user.age} years old.`;
+    if (userId < users.length) {
+      let user = users[userId];
+      const username = user.Name;
+      const userAge = user.age;
+      console.log(user.Name);
+      userDetails.innerText = `${username} is ${userAge} years old.`;
+    } else {
+      userDetails.innerText = `Loading...`;
+      setTimeout(() => {
+        userDetails.innerText = `Please check the user ID you have entered.`;
+      }, 1000);
+    }
   } catch (e) {
     window.location = "./error.html";
   }
 }
-
-// function getUser(userId) {
-//   try {
-//     if (!isNaN(Number(userId)) && Number(userId) < users.length) {
-//       let user = users[userId];
-//       console.log(user.name);
-//       userDetails.innerText = `${user.name} is ${user.age} years old.`;
-//     } else {
-//       userDetails.innerText = `Please check the user ID you have entered.`;
-//     }
-//   } catch (e) {
-//     window.location = "./error.html";
-//   }
-// }
-
-// function getUser(userId) {
-//   try {
-//     if (userId && !isNaN(Number(userId)) && Number(userId) < users.length) {
-//       let user = users[userId];
-//       console.log(user.name);
-//       userDetails.innerText = `${user.name} is ${user.age} years old.`;
-//     } else {
-//       userDetails.innerText = `Please check the user ID you have entered.`;
-//     }
-//   } catch (e) {
-//     window.location = "./error.html";
-//   }
-// }
